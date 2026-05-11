c8f662c85431d445fbdfd49be3d340fd4d5203c1
Ripujit G N <narasimharipujit@gmail.com>
2026-05-11 00:15:41 -0700

    Defined two functions inside the MCP as tools that an agent can tap into when needed.

--- postgres-mcp-server/main.py
+++ postgres-mcp-server/main.py
@@ -1,8 +1,12 @@
-from typing import List, Dict
+from typing import List, Dict, Any
 import os
+import re
 import psycopg2
 from mcp.server.fastmcp import FastMCP
 from dotenv import load_dotenv
+
+# A list of forbidden SQL operations
+forbidden = re.compile(r"\b(INSERT|UPDATE|DELETE|DROP|TRUNCATE|ALTER|CREATE|GRANT|REVOKE)\b", re.IGNORECASE)
 
 # Load environment variables from .env file
 load_dotenv()
@@ -26,12 +30,38 @@ DB_CONFIG = {
 #  - Return the rows as a list of dictionaries (column_name → value)
 # Hint: Use the same psycopg2 connection pattern shown in `get_schema`.
 
+@mcp.tool()
+async def execute_sql(sql: str) -> List[Dict[str, Any]]:
+    """Execute a SQL query and return the result."""
+    
+    if forbidden.search(sql):
+        raise ValueError("Only SELECT queries are allowed. You are trying to execute a forbidden operation.")
+    with psycopg2.connect(**DB_CONFIG) as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql)
+            columns = [desc.name for desc in cur.description]
+            rows = [dict(zip(columns,r)) for r in cur.fetchall()]
+    return rows
 
 # TODO: Implement a third MCP tool called `list_tables`
 # This function should:
 #  - Take no inputs
 #  - Return the list of table names available in the current database
 # Hint: Query `information_schema.tables` and filter for `table_schema = 'public'`.
+
+@mcp.tool()
+async def list_tables() -> List[str]:
+    """Return the list of table names available in the current database."""
+    sql = """
+        SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema = 'public'
+    """
+    with psycopg2.connect(**DB_CONFIG) as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql)
+            rows = [r[0] for r in cur.fetchall()]
+    return rows
 
 @mcp.tool()
 async def get_schema(table: str) -> List[Dict]:
